5cfb266ce42baf8e31a511c699a567065ef984ac
Dexter Antonio <dexter.d.antonio@gmail.com>
2021-01-16 21:51:21 -0800

    added superuser

--- website/study_ai/courses/admin.py
+++ website/study_ai/courses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from courses.models import Course, Achievment, Major
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    pass
+
+class MajorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Achievment, AchievementAdmin)
+admin.site.register(Major, MajorAdmin)
